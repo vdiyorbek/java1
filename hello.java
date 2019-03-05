c8cc8032206a81e215e87f487b825a6ac4c0fca6
vdiyorbek <vdiyorbek@gmail.com>
2019-03-05 22:13:04 +0500

    vdiyorbek

--- hello.java
+++ hello.java
@@ -1,10 +1,13 @@
 import java.util.Scanner;
 
 public class hello {
+
   public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in);
-    System.out.println("a n2i kirit:");
-    int a = scan.nextInt();
-    System.out.print("Kiritish:" + a);
+    int no_friends;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter values");
+    no_friends = sc.nextInt();
+    sc.close();2
+    System.out.println("Value entered is" + no_friends);
   }
 }
